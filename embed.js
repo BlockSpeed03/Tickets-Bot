d15cd86d66e6ee3fa52c9e3e073012cc9659ff82
BlockSpeed03 <BlockySpeed@gmail.com>
2020-02-24 19:45:45 -0600

    Delete embed.js

--- embed.js
+++
@@ -1,6 +0,0 @@
-    const Discord = require('discord.js')
-    const help = new Discord.RichEmbed()
-            .setTitle('Ticket Commands')
-            .setColor('#b53235')
-            .setDescription('List of commands')
-            .addField('test', '!new');
