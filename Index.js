2f487f7dcf07ea2d42d9427119473ee8d41df998
BlockSpeed03 <BlockySpeed@gmail.com>
2020-02-26 20:41:33 -0600

    Update to V12.X Master

--- Index.js
+++ Index.js
@@ -5,22 +5,24 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 
 
-const ticket = new Discord.RichEmbed()
+const ticket = new Discord.MessageEmbed()
             .setTitle('Open A Ticket')
             .setColor('#b53235')
             .setDescription('Please Tell Us The Problem You Are Having!');
 
-const help = new Discord.RichEmbed()
+const help = new Discord.MessageEmbed()
             .setTitle('Ticket Commands')
             .setColor('#b53235')
             .setDescription('List of commands')
-            .addField('\u200b', '!new');
+            .addFields(
+                { name: '\u200b', value: '!new', inline: false }
+                );
 
 const prefix = (config.prefix)
 
 const supportrole = (config.supportRoleid)
 client.on('ready', () => {
-    client.channels.get("680316816156786694").send("Bot Active! Waiting to react to commands!")
+    client.channels.cache.get("680316816156786694").send("Bot Active! Waiting to react to commands!")
     client.user.setActivity("Testing Bot", {type:"WATCHING"})
 })
 
@@ -28,22 +30,28 @@ client.on('ready', () => {
 /*Commands*/
 client.on('message', (message) => {
     if(message.author.bot) return;
-    if(!message.guild.channels.find(x => x.name === message.author)) {
+    if(!message.guild.channels.cache.find(x => x.name === message.author)) {
     if(message.content == (prefix + "new") || message.channel.id == config.ticketChannel) {
 
         message.channel.send(ticket).then(() => {
             const filter = m => m.content == (prefix + "new") || m.author.id === message.author.id;
-
             message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
             .then(collected => {
                 message.channel.send(config.ticketOpen)
-                message.guild.createChannel(message.author.username, { 
+                
+                message.guild.channels.create(message.author.username, { 
                     type: `text`, 
                     topic: collected.first().content.toLowerCase(), 
-                    parent: config.category})
+                    parent: config.category,
+                    permissionOverwrites: [
+                        {
+                            id: message.author.id,
+                            allow: ['VIEW_CHANNEL'],
+                        }
+                    ]})
                     .then((created) => {
-                    message.guild.channels.find(channel => channel.name === message.author.username)
-                    created.send(message.author + ' ' + config.message)
+                    message.guild.channels.cache.find(channel => channel.name === message.author.username)
+                    created.send(message.author.username + ' ' + config.message)
                     })
                 })
 
@@ -53,7 +61,7 @@ client.on('message', (message) => {
         message.send(config.open)      
         console.log("test");      
         }
-    if(message.guild.roles.get(supportrole)) {
+    if(message.guild.roles.cache.get(supportrole)) {
         
     }
     
